bbfc1f83f6f2ecd2dd4ce84cbfa7159295c7e303
Kitlith <kitlith@kitl.pw>
2017-11-02 20:56:18 -0700

    Fix everything!

--- source/menu.c
+++ source/menu.c
@@ -6,6 +6,7 @@
 void arbitrary_menu(void) {
     char clear_text[64] = { 0 };
     uint8_t command[8] = {0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
+    uint8_t response[4] = {0};
     uint8_t value;
     int cursor_pos = 0;
 
@@ -16,14 +17,14 @@ void arbitrary_menu(void) {
                         command[4], command[5], command[6], command[7]);
         DrawStringF(10, 20, true, clear_text);
         DrawStringF(10 + 8*cursor_pos, 20, true, "^");
+        DrawStringF(10, 40, true, "%02X%02X%02X%02X",
+                    response[0], response[1], response[2], response[3]);
 
         u32 pad_state = InputWait();
         if (pad_state & (BUTTON_B))
             break;
         if (pad_state & BUTTON_A) {
-            uint32_t result;
-            cardPolledTransfer(CARD_ACTIVATE, &result, 4, command);
-            DrawStringF(10, 40, true, "%08X", result);
+            cardPolledTransfer(CARD_ACTIVATE | CARD_BLK_SIZE(7), (uint32_t*)response, 4, command);
         }
 
         if (pad_state & BUTTON_UP) {
@@ -53,23 +54,23 @@ void arbitrary_menu(void) {
 static const uint8_t cmdReadSerial[8] = {0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void serial_menu(void) {
-    char recieved_str[256] = {0};
-    unsigned index = 0;
+    unsigned charno = 1;
     unsigned lineno = 1;
     while (true) {
         if (CheckButton(BUTTON_B))
             break;
 
         uint8_t result[4];
-        cardPolledTransfer(CARD_ACTIVATE, (uint32_t *)result, 4, cmdReadSerial);
-        if (result[0] & 1) {
-            if (result[3] == '\n' || result[3] == '\r') {
-                recieved_str[index] = '\0';
-                index = 0;
-                DrawString(TOP_SCREEN, recieved_str, 10, lineno++ * 10, STD_COLOR_FONT, STD_COLOR_BG);
-            } else {
-                recieved_str[index++] = result[3];
+        cardPolledTransfer(CARD_ACTIVATE | CARD_BLK_SIZE(7), (uint32_t *)result, 4, cmdReadSerial);
+        // bool data_present = result[0] & 1;
+        uint8_t data = result[3];
+        if (data != 0x00 && data != 0xFF) {
+            if (data == '\n' || data == '\r') {
+                ++lineno;
+                charno = 1;
+                continue;
             }
+            DrawCharacter(TOP_SCREEN, data, charno++ * 10, lineno * 10, STD_COLOR_FONT, STD_COLOR_BG);
         }
     }
 }
